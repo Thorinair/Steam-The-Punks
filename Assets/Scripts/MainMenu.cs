24596ee2c495d0a24eddf28f824d427ae502a54b
Davor Tomljenovic <tomljenovicd@yahoo.com>
2016-08-12 13:53:52 +0200

    Added main menu,credits,jukebox music

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    void OnMouseUp()
+    {
+        print("clicked");
+        if (this.tag == "play")
+            SceneManager.LoadScene("Level1");
+        else if (this.tag == "about")
+            SceneManager.LoadScene("Credits");
+        else if (this.tag == "quit")
+            Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScrollingText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.up * Time.deltaTime*10);
+        if (this.transform.position.y > 500)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("MainMenu");
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("MainMenu");
+        }
+	}
+}
