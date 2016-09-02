40d1e88af92e81259e744620ce599f93e9781929
Davor Tomljenovic <tomljenovicd@yahoo.com>
2016-09-02 17:48:28 +0200

    Robot controls

---
+++ Assets/characterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterController : MonoBehaviour {
+
+    public float translateSpeed = 10f;
+    public float rotateSpeed = 100f;
+
+	// Use this for initialization
+	void Start () {
+        //Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * translateSpeed;
+        float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+	}
+}
---
+++ Assets/weaponController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weaponController : MonoBehaviour {
+
+    Vector2 mouseAim;
+    Vector2 smoothV;
+
+    public float sensitivity = 0.1f;
+    public float smoothing = 1f;
+    public float damage = 5f;
+    public float radius = 0f;
+
+    public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+
+        mouseAim += smoothV;
+
+        this.transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(mouseAim.x,-20,25), this.transform.up);
+
+        if (Input.GetMouseButton(0))
+        {
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+
+            }
+        }
+    }
+}
