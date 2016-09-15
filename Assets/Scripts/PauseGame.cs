ba7034ad546af27ffebd9cefd8aa3645584920c2
Davor Tomljenovic <tomljenovicd@yahoo.com>
2016-09-15 14:08:52 +0200

    Removed aoe script, added in-game menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+    private bool menuToggle=false;
+    public GameObject menuScreen;
+
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            menuToggle=!menuToggle;
+            if (menuToggle == false)
+            {
+                menuScreen.SetActive(false);
+            }
+            else
+            {
+                menuScreen.SetActive(true);
+            }
+        }
+              
+	}
+
+    public void Resume()
+    {
+        menuToggle=false;
+        menuScreen.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
