aba1eecd41a618e563c463ca4dfc281a2d661379
Thorinair <thorinair@yahoo.com>
2016-09-15 16:26:03 +0200

    Added camera bounderies.

--- Assets/Scripts/ScrollingCamera.cs
+++ Assets/Scripts/ScrollingCamera.cs
@@ -7,12 +7,17 @@ public class ScrollingCamera : MonoBehaviour {
     public float speed = 0.5f;
     public bool startMoving;
     Vector3 direction;
+    int side = 0;
 	
 	// Update is called once per frame
 	void Update () {
-        if (startMoving)
-        {
-            camera.position += direction;
+        if (startMoving) {
+            if ((camera.position.z < 330 && side == 1) ||
+                (camera.position.z > 70 && side == 2) ||
+                (camera.position.x < -120 && side == 3) ||
+                (camera.position.x > -360 && side == 4)) {
+                camera.position += direction;
+            }
         }
 	}
     public void StartMovingForward(bool opposite)
@@ -20,12 +25,14 @@ public class ScrollingCamera : MonoBehaviour {
         startMoving = true;
         direction = (!opposite) ? camera.forward : -camera.forward;
         direction *= speed;
+        side = (!opposite) ? 1 : 2;
     }
     public void StartMovingSides(bool opposite)
     {
         startMoving = true;
         direction = (!opposite) ? camera.right : -camera.right;
         direction *= speed;
+        side = (!opposite) ? 3 : 4;
     }
     public void StopMoving()
     {
